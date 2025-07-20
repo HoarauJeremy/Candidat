24cc2e755fcaac0ea6603eb02a4833f0b8494c8b
Jérémy Hoarau <jeremyhoarau44@gmail.com>
2025-07-20 20:29:39 +0400

    Modification des routes Candidature.tsx et ajout de routes.ts pour faciliter les imports

--- src/routes/Candidature.tsx
+++ src/routes/Candidature.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Index from "@Components/Candidature/index.tsx";
 import List from "@Components/Candidature/List.tsx";
+import Show from "@Components/Candidature/Show.tsx";
 
 const Candidature = () => {
   return (
@@ -8,6 +9,7 @@ const Candidature = () => {
       <Routes>
         <Route element={<Index />}>
           <Route index element={<List />}/>
+          <Route path=":id" element={<Show />}/>
         </Route>
       </Routes>
     </>
---
+++ src/routes/routes.ts
@@ -0,0 +1,9 @@
+export {default as Candidature} from "@Routes/Candidature";
+export {default as Commune} from "@Routes/Commune.tsx";
+export {default as Document} from "@Routes/Document.tsx";
+export {default as Metier} from "@Routes/Metier.tsx";
+export {default as OffreEmploie} from "@Routes/OffreEmploie.tsx";
+export {default as Organisation} from "@Routes/Organisation.tsx";
+export {default as Utilisateur} from "@Routes/Utilisateur.tsx";
+export {default as Layout} from "@Components/Accueil/Layout.tsx";
+export {default as Accueil} from "@Components/Accueil/Accueil.tsx";
