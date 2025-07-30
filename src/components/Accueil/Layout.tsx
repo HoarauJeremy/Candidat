95d1582644f3276813a181cc829743b3a4561688
jeremy <jeremyhoarau44@gmail.com>
2025-07-30 20:23:11 +0400

    Modification du style dans Layout.tsx

--- src/components/Accueil/Layout.tsx
+++ src/components/Accueil/Layout.tsx
@@ -6,7 +6,8 @@ const Layout = () => {
     <>
       <Header />
 
-      <main className="w-full h-auto max-w-11/12 mx-auto mt-20 py-4 md:p-4">
+      <main className="w-full h-auto max-w-11/12 mx-auto mt-32 md:mt-20 py-4 md:p-4">
+      {/*<main className="w-full h-auto max-w-11/12 mx-auto mt-20 py-4 md:p-4">*/}
       {/*<main className="w-full minh-screen flex flex-col bg-neutral-50 text-neutral-600">*/}
         <Outlet />
       </main>
